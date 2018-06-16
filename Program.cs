8f308c42f090a0275695ed8247014cbee35a797c
Peefy <xpf6677@163.com>
2018-06-16 22:29:50 +0800

    renew

--- Program.cs
+++ Program.cs
@@ -6,8 +6,10 @@ namespace PeefyLeetCode
     {
         static void Main(string[] args)
         {
-            var solution = new CountAndSay.Solution();
-            Console.WriteLine(solution.CountAndSay(3));
+            var solution1 = new TwoSum.Solution();
+            Console.WriteLine(solution1.TwoSum(new int[]{1,2,3,4}, 7));
+            var solution2 = new CountAndSay.Solution();
+            Console.WriteLine(solution2.CountAndSay(5));
             Console.WriteLine("Hello World!");
         }
     }
