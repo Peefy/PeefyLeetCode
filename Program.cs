ca42008c5858b9865d66f0af0cd73da87b94b1c9
Peefy <xpf6677@163.com>
2018-06-18 17:27:17 +0800

    update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace PeefyLeetCode
 {
@@ -6,10 +7,35 @@ namespace PeefyLeetCode
     {
         static void Main(string[] args)
         {
-            var solution1 = new TwoSum.Solution();
-            Console.WriteLine(solution1.TwoSum(new int[]{1,2,3,4}, 7));
-            var solution2 = new CountAndSay.Solution();
-            Console.WriteLine(solution2.CountAndSay(5));
+            var filename = "test.md";
+            var i = 1;
+            var j = 13;
+
+            
+
+            while (i <= 10)
+            {
+                if (i == 1)
+                {
+                    File.AppendAllText(filename, $"## Problem {j}1 - Problem {j + 1}0\r\n\r\n");
+                }
+                if (i < 10)
+                {
+File.AppendAllText(filename, $"* #### Problem {j}{i} (Easy)\r\n");
+                }
+                
+                if (i == 10)
+                {
+                    ++j;
+                    File.AppendAllText(filename, $"* #### Problem {j}0 (Easy)\r\n");
+                    File.AppendAllText(filename, "\r\n");
+                    i = 0;
+                    if(j >= 65)
+                        break;
+                }
+                ++i;
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
