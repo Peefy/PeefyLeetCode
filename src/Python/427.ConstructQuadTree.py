0557c7d8499b65df1467e6ddd5d40cd8abfde35c
Peefy <xpf6677@163.com>
2018-09-07 23:08:55 +0800

    Add some

--- src/Python/427.ConstructQuadTree.py
+++ src/Python/427.ConstructQuadTree.py
@@ -8,33 +8,50 @@ class Node:
         self.bottomRight = bottomRight
 
 class Solution:
-    def construct_node(self, node, grid):
-        node = Node('*', True, None, None, None, None, None)
-        n = len(grid)
-        false_flag = 0
-        true_flag = 0
-        for i in range(n):
-            for j in range(n):
-                if grid[i][j] == 0:
-                    node.val = False
-                    false_flag = 1
-                else:
-                    node.val = True
-                    true_flag = 1
-        if true_flag and false_flag:
-            node.isLeaf = False
+    def construct_div(self, grid, left, top, width):
+        if width == 1:
+            if grid[top][left] == 1:
+                return Node(True, True, None, None, None, None)
+            return Node(False, True, None, None, None, None)
+        if width == 2:
+            if grid[top][left] == 1 and grid[top + 1][left] == 1 and grid[top][left + 1] == 1 and grid[top + 1][left + 1] == 1:
+                return Node(True, True, None, None, None, None)
+            if grid[top][left] == 0 and grid[top + 1][left] == 0 and grid[top][left + 1] == 0 and grid[top + 1][left + 1] == 0:
+                return Node(False, True, None, None, None, None)
+        half_width = width // 2
+        grid_1 = left, top, half_width
+        grid_2 = left + half_width, top, half_width
+        grid_3 = left, top + half_width, half_width
+        grid_4 = left + half_width, top + half_width, half_width
+        topleft = self.construct_div(grid, grid_1[0], grid_1[1], grid_1[2])
+        topright = self.construct_div(grid, grid_2[0], grid_2[1], grid_2[2])
+        bottomleft = self.construct_div(grid, grid_3[0], grid_3[1], grid_3[2])
+        bottomright = self.construct_div(grid, grid_4[0], grid_4[1], grid_4[2])
+        node = Node(True, True, None, None, None, None)
+        if topleft.val == True and topright.val == True and bottomleft.val == True and bottomright.val == True:
+            node.val = True
+            node.isLeaf = True
+        if topleft.val == False and topright.val == False and bottomleft.val == False and bottomright.val == False:
+            node.val = False
+            node.isLeaf = True
+        node.topLeft = topleft
+        node.topRight = topright
+        node.bottomLeft = bottomleft
+        node.bottomRight = bottomright
+        node.isLeaf = False
         return node
-
+        
     def construct(self, grid):
         """
         :type grid: List[List[int]]
         :rtype: Node
         """
-        root = Node('*', False, None, None, None, None, None)
-        return self.construct_node(root, grid)
+        width = len(grid)
+        root = self.construct_div(grid, 0, 0, width)
+        return root
 
 if __name__ == '__main__':
     solution = Solution()
-    print(solution.construct([[1, 1], [1, 1]]))
+    print(solution.construct([[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,1,1,1,1],[1,1,1,1,1,1,1,1],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0],[1,1,1,1,0,0,0,0]]))
 else:
     pass
