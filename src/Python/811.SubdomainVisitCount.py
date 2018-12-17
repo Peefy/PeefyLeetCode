7db949bc531199fa6a0b550c9e33c00ba7407cad
Peefy <xpf6677@163.com>
2018-12-17 21:19:36 +0800

    Update 811.SubdomainVisitCount.py

---
+++ src/Python/811.SubdomainVisitCount.py
@@ -0,0 +1,17 @@
+
+class Solution:
+    def subdomainVisits(self, cpdomains):
+        """
+        :type cpdomains: List[str]
+        :rtype: List[str]
+        """
+        pass
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.subdomainVisits(["9001 discuss.leetcode.com"]))
+    print(solution.subdomainVisits(["900 google.mail.com", "50 yahoo.com", "1 intel.mail.com", "5 wiki.org"]))
+else:
+    pass
+
+
