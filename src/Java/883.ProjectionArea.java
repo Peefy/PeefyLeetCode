0afaf679a46a833ec1d01e351bc47502bdfe74ff
Peefy <xpf6677@163.com>
2018-12-23 12:59:38 +0800

    Update 883.ProjectionArea.java

---
+++ src/Java/883.ProjectionArea.java
@@ -0,0 +1,31 @@
+
+import java.awt.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null)
+        {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
