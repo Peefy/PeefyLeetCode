315f78657f18ea92950c76b15064f00e5a254811
Peefy <xpf6677@163.com>
2018-12-25 22:18:32 +0800

    Update 893.NumSpecialEquivGroups.py

---
+++ src/Python/893.NumSpecialEquivGroups.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def numSpecialEquivGroups(self, A):
+        """
+        :type A: List[str]
+        :rtype: int
+        """
+        pass
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    print(solution.numSpecialEquivGroups(["a","b","c","a","c","c"]))
+    print(solution.numSpecialEquivGroups(["aa","bb","ab","ba"]))
+    print(solution.numSpecialEquivGroups(["abc","acb","bac","bca","cab","cba"]))
+    print(solution.numSpecialEquivGroups(["abcd","cdab","adcb","cbad"]))
+else:
+    pass
