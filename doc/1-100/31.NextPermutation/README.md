046764cc07f1e17318ceb79277a42114ae44c0f8
Peefy <xpf6677@163.com>
2019-02-19 22:26:41 +0800

    Finish problem 31 python

--- doc/1-100/31.NextPermutation/README.md
+++ doc/1-100/31.NextPermutation/README.md
@@ -31,7 +31,35 @@
 
 ```python
 
+class Solution:
+    def reverse(self, nums, l, r):
+        while l < r:
+            tmp = nums[l]
+            nums[l] = nums[r]
+            nums[r] = tmp
+            l += 1
+            r -= 1
 
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        nlen = len(nums)
+        i = nlen - 1
+        while i > 0 and nums[i] <= nums[i - 1]:
+            i -= 1
+        if i == 0:
+            self.reverse(nums, 0, nlen - 1)
+            return
+        d1 = i - 1
+        i = nlen - 1
+        while nums[i] <= nums[d1]:
+            i -= 1
+        d2 = i
+        nums[d1], nums[d2] = nums[d2], nums[d1]
+        self.reverse(nums, d1 + 1, nlen - 1)
+        return
 
 ```
 
---
+++ src/Python/31.NextPermutation.py
@@ -0,0 +1,43 @@
+class Solution:
+    def reverse(self, nums, l, r):
+        while l < r:
+            tmp = nums[l]
+            nums[l] = nums[r]
+            nums[r] = tmp
+            l += 1
+            r -= 1
+
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        nlen = len(nums)
+        i = nlen - 1
+        while i > 0 and nums[i] <= nums[i - 1]:
+            i -= 1
+        if i == 0:
+            self.reverse(nums, 0, nlen - 1)
+            return
+        d1 = i - 1
+        i = nlen - 1
+        while nums[i] <= nums[d1]:
+            i -= 1
+        d2 = i
+        nums[d1], nums[d2] = nums[d2], nums[d1]
+        self.reverse(nums, d1 + 1, nlen - 1)
+        return
+            
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.nextPermutation([1, 2, 3]))
+    print(solution.nextPermutation([3, 2, 1]))
+    print(solution.nextPermutation([1, 3, 2]))
+    print(solution.nextPermutation([1, 2, 1]))
+    print(solution.nextPermutation([1, 2]))
+    print(solution.nextPermutation([3, 1, 2]))
+    print(solution.nextPermutation([1, 3, 2]))
+    print(solution.nextPermutation([5, 5, 2, 3, 4, 7]))
+    print(solution.nextPermutation([5, 4, 7, 5, 3, 2]))
+    
