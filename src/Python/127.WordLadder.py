6ae2c9da8423450a79b94b86d3ea69b2717b7e85
Peefy <xpf6677@163.com>
2019-06-21 22:53:48 +0800

    Update 127.WordLadder.py

---
+++ src/Python/127.WordLadder.py
@@ -0,0 +1,10 @@
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.minimumTotal([[2],[3,4],[6,5,7],[4,1,8,3]]))
+
