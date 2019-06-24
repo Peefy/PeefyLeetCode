d1e440373ffa656400de9a44a7bad72a299a2a12
Peefy <xpf6677@163.com>
2019-06-24 18:44:47 +0800

    Update

--- src/Python/127.WordLadder.py
+++ src/Python/127.WordLadder.py
@@ -1,11 +1,13 @@
-
 class Solution:
     def ladderLength(self, beginWord, endWord, wordList):
-        pass
+        s = set(wordList)
 
 
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.ladderLength("hit","cog",["hot","dot","dog","lot","log","cog"]))
-    print(solution.ladderLength("hit","cog",["hot","dot","dog","lot","log"]))
-
+    print(
+        solution.ladderLength("hit", "cog",
+                              ["hot", "dot", "dog", "lot", "log", "cog"]))
+    print(
+        solution.ladderLength("hit", "cog",
+                              ["hot", "dot", "dog", "lot", "log"]))
