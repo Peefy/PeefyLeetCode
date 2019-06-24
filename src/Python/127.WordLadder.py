49ec2acfb415fbb5f1d17fe22706f74ba91b9b80
Peefy <xpf6677@163.com>
2019-06-24 23:00:03 +0800

    Update 127.WordLadder.py

--- src/Python/127.WordLadder.py
+++ src/Python/127.WordLadder.py
@@ -1,6 +1,26 @@
+
 class Solution:
-    def ladderLength(self, beginWord, endWord, wordList):
-        s = set(wordList)
+
+    def bfs(self, beginWord, endWord, wordList, first):
+        for word in wordList:
+            if visited == 1:
+                continue
+            for i in range(len(first)):
+                if first[i] != word[i]:
+                    diff += 1
+            visited[i] = 1
+            if diff == 1:
+                first = 
+                self.count += 1
+                self.bfs(beginWord, endWord, wordList, first)
+
+    def ladderLength(self, beginWord, endWord, wordList, first):
+        self.count = 0
+        first = beginWord
+        self.visited = [0] * len(wordList)
+        for word in wordList:
+
+        return self.count
 
 
 if __name__ == "__main__":
