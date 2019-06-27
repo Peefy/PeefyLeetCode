2aed023626ed89d48826f4f22903192834c3be3f
Peefy <xpf6677@163.com>
2019-06-27 08:44:44 +0800

    Update 130.SurroundedRegions.py

---
+++ src/Python/130.SurroundedRegions.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def solve(self, board):
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.solve([
+        ['X', 'X', 'X', 'X'],
+        ['X', 'O', 'O', 'X'],
+        ['X', 'X', 'O', 'X'],
+        ['X', 'O', 'X', 'X'],
+    ]))
+        
