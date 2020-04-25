2e36043de51cb5d42a22e98875fca61aba0fc1c9
Peefy <xpf6677@163.com>
2020-04-25 17:53:02 +0800

    Create 4.25.com3.py

---
+++ src/Competition/4.25.com3.py
@@ -0,0 +1,11 @@
+
+class Solution:
+    def expectNumber(self, scores):
+        pass
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.numSquares(12))
+    print(solution.numSquares(13))
+
